931807743511712ccdb02a20913fb266862227e4
crimsonneuron <james.r.devereux@gmail.com>
2025-12-16 22:57:47 -0500

    most of day 4

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -18,18 +18,32 @@ fn main() {
     println!("{sum}");
 }
 
-fn find_biggest_battery(vec: Vec<u32>) -> u32 {
-    let mut x_1_index = 0;
-    for i in 0..vec.len()-1 {
-        if vec[i]>vec[x_1_index] {
-            x_1_index = i
+fn find_biggest_battery(vec: Vec<u32>) -> u64 {
+    let mut values= [0;12];
+    let mut last_index:isize = -1;
+    for i in (1..=12).rev() {
+        for j in last_index+1..((vec.len()-(i-1)) as isize) {
+            if vec[j as usize] >values[12-i] {
+                values[12-i] = vec[j as usize];
+                last_index = j;
+            }
         }
     }
-    let mut x_2_index = x_1_index+1;
-    for j in x_1_index+1..vec.len() {
-        if vec[j]>vec[x_2_index] {
-            x_2_index =j 
-        }
-    }
-    vec[x_1_index]*10 + vec[x_2_index]
+
+    values.into_iter().fold(0, |acc, x| acc*10+(x as u64))
+
+
 }
+
+
+//for each digit placement (12 through 1), we go through the vector till there are that number
+//of digits left -1 (because we always need some more digits afterwards), and we keep track of the
+//biggest index found thus far. we mark it down
+//current problem is: it is always comparing to index 0, which might just be 9 (i.e., it'll never
+//update). 
+//So how do I solve this? just another if-statement? i.e., if index ==0, just compare to 0?
+//what I want to do here is very simple: go through the vector of the relevant length and for each
+//one, find the biggest value.
+//The only problem is carrying the things through to the next repetition, because the next search
+//should start from where the previous search ended.
+//Ok, so just keep both values (instead of this inanity)
---
+++ src/bin/day04.rs
@@ -0,0 +1,85 @@
+use std::collections::HashMap;
+fn main() {
+    //let input = include_str!("../input/day04.txt");
+     let input = "..@@.@@@@.
+ @@@.@.@.@@
+ @@@@@.@.@@
+ @.@@@@..@.
+ @@.@@@@.@@
+ .@@@@@@@.@
+ .@.@.@.@@@
+ @.@@@.@@@@
+ .@@@@@@@@.
+ @.@.@@@.@.";
+    let mut map: HashMap<(i32, i32), char> = HashMap::new();
+    for (row_index, line) in input.lines().enumerate() {
+        for (column_index, char) in line.chars().enumerate() {
+            map.insert((column_index as i32, row_index as i32), char);
+        }
+    }
+    //the map is now initialized so that we're basically working exclusively in quadrant IV
+    //we're doing <x,y> stuff here exclusvie
+    println!("len is: {}", get_removable(&map).len());
+    println!("Says we can remove: {:?}", get_removable(&map))
+//    let mut counter = 0;
+//    let mut removable_targets =get_removable(&map);
+//    while !removable_targets.is_empty() {
+//        counter+=removable_targets.len();
+//        remove(&mut map,removable_targets);
+//        removable_targets = get_removable(&map);
+//    }
+//    println!("Counter says: {counter}");
+
+
+}
+
+fn count_removable(map: &HashMap<(i32, i32), char>) -> i32 {
+    let directions:[(i32, i32);8] = [
+    (-1,1),    (0,1),  (1,1),
+    (-1, 0),           (1, 0),
+    (-1, -1), (0, -1), (1, -1)
+    ];
+
+    let mut free_count = 0;
+    for element in map {
+        let mut roll_count =0;
+        if element.1 != &'@' {
+            continue
+        }
+        for direction in directions {
+            if map.get(&(((element.0.0 as i32)+direction.0) , ((element.0.1 as i32)+direction.1) )) == Some(&'@') {
+                roll_count+=1;
+            }
+        }
+        if roll_count <4 {
+            free_count+=1;
+        }
+    }
+    free_count
+}
+fn get_removable(map: &HashMap<(i32, i32), char>) -> Vec<(i32,i32)> {
+    const DELTAS:[(i32, i32);8] = [
+    (-1,1),    (0,1),  (1,1),
+    (-1, 0),           (1, 0),
+    (-1, -1), (0, -1), (1, -1)
+    ];
+    
+    map.iter()
+        .filter(|(_, char)| **char!='@')
+        .filter(|((x,y), _)|{
+            let roll_count = DELTAS.iter()
+                .filter(|(dx, dy)| map.get(&(x+dx, y+dy)) == Some(&'@'))
+                .count();
+            
+            roll_count <4
+        })
+        .map(|((x,y),_)| (*x,*y))
+        .collect::<Vec<(i32, i32)>>()
+     
+}
+
+fn remove(map: &mut HashMap<(i32, i32), char>, targets: Vec<(i32, i32)>) {
+    for target in targets {
+        map.entry(target).and_modify(|char|*char = '.');
+    }
+}
